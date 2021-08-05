8b388ca4c79f0ff83a02b320c9257c3df57ba8f9
Nuwan Naththandige <nuwannc2@hotmail.com>
2021-08-06 02:27:05 +1200

    Call more nodeTypes in stmts

--- src/nodetypes/_stmt_class_method.py
+++ src/nodetypes/_stmt_class_method.py
@@ -23,8 +23,11 @@ def stmt_class_method(self, node, parent_node, parent_node_type, relationship_ty
     # Describes the statements inside the method
     if node['stmts']:
         for stmt in node['stmts']:
-            if stmt['nodeType'] == 'Stmt_Expression':
+            if stmt['nodeType'] == 'Stmt_Global':
+                self.stmt_global(stmt['vars'], node['name']['name'], '{scope}:CLASS_METHOD'.format(scope=scope), '{scope}:{class_method}'.format(scope=scope, class_method=node['name']['name']))
+            elif stmt['nodeType'] == 'Stmt_Expression':
                 self.stmt_expression(stmt['expr'], node['name']['name'], '{scope}:CLASS_METHOD'.format(scope=scope), '{scope}:{class_method}'.format(scope=scope, class_method=node['name']['name']))
             elif stmt['nodeType'] == 'Stmt_If':
                 self.stmt_if(stmt, node['name']['name'], '{scope}:CLASS_METHOD'.format(scope=scope), '{scope}:{class_method}'.format(scope=scope, class_method=node['name']['name']))
-            
+            elif stmt['nodeType'] == 'Stmt_Return':
+                self.stmt_return(stmt['expr'], node['name']['name'], '{scope}:CLASS_METHOD'.format(scope=scope), '{scope}:{class_method}'.format(scope=scope, class_method=node['name']['name']))
