49c2f01b2c374edfda8ac482186277bcaf0f4d33
Nuwan Naththandige <nuwannc2@hotmail.com>
2021-08-06 02:24:45 +1200

    Add new nodeTypes

--- src/nodetypes/init.py
+++ src/nodetypes/init.py
@@ -1,6 +1,9 @@
 class NodeType:
     from nodetypes._expr_array_dim_fetch import expr_array_dim_fetch
     from nodetypes._expr_func_call import expr_func_call
+    from nodetypes._expr_include import expr_include
+    from nodetypes._expr_method_call import expr_method_call
+    from nodetypes._expr_static_call import expr_static_call
     from nodetypes._expr_variable import expr_variable
     from nodetypes._filename_node import filename_node
     from nodetypes._scalar_encapsed import scalar_encapsed
@@ -14,6 +17,7 @@ class NodeType:
     from nodetypes._stmt_if import stmt_if
     from nodetypes._stmt_namespace import stmt_namespace
     from nodetypes._stmt_property import stmt_property
+    from nodetypes._stmt_return import stmt_return
     from nodetypes._stmt_while import stmt_while
     from nodetypes._support import generate_block_name_by_line
 
