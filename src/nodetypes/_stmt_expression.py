cf5a2d2d2a047675c99bfb56bf53ce8b1f5865b4
Nuwan Naththandige <nuwannc2@hotmail.com>
2021-08-06 02:28:56 +1200

    Add new expression types, alter Expr_FuncCall

--- src/nodetypes/_stmt_expression.py
+++ src/nodetypes/_stmt_expression.py
@@ -28,7 +28,10 @@ def stmt_expression(self, expr, parent_node, parent_node_type, scope):
                 pass
             # If the value of the above variable is a "Function call"
             elif expr['expr']['nodeType'] == 'Expr_FuncCall':
-                self.expr_func_call(expr['expr'], expr['var']['name'], '{scope}:VARIABLE'.format(scope=parent_node), scope)
+                if expr['var']['nodeType'] == 'Expr_PropertyFetch':
+                    self.expr_func_call(expr['expr'], expr['var']['name']['name'], '{scope}:VARIABLE'.format(scope=scope), scope, 'ASSIGNS')
+                else:
+                    self.expr_func_call(expr['expr'], expr['var']['name'], '{scope}:VARIABLE'.format(scope=scope), scope, 'ASSIGNS')
             # If the value of the above variable is similar to "$GET['id']"
             elif expr['expr']['nodeType'] == 'Expr_ArrayDimFetch':
                 self.expr_array_dim_fetch(expr['expr'], expr['var']['name'], '{scope}:VARIABLE'.format(scope=parent_node), 'ASSIGNS', scope, expr['expr']['var']['name'])
@@ -37,7 +40,9 @@ def stmt_expression(self, expr, parent_node, parent_node_type, scope):
                 self.scalar_encapsed(expr['expr'], expr['var']['name'], '{scope}:VARIABLE'.format(scope=parent_node), 'ASSIGNS', scope)
 
     elif expr['nodeType'] == 'Expr_FuncCall':
-        self.expr_func_call(expr, parent_node, parent_node_type, scope)
+        self.expr_func_call(expr, parent_node, parent_node_type, scope, 'FUNCTION_CALL')
+    elif expr['nodeType'] == 'Expr_MethodCall':
+        self.expr_method_call(expr, parent_node, parent_node_type, scope)
     elif expr['nodeType'] == 'Expr_AssignOp_Concat':
         if expr['var']['nodeType'] == 'Expr_Variable':
             # Create the variable and its relationship with the parent node
@@ -48,3 +53,7 @@ def stmt_expression(self, expr, parent_node, parent_node_type, scope):
             # If the value of the above variable is similar to "<pre>Hello ${name}</pre>";
             if expr['expr']['nodeType'] == 'Scalar_Encapsed':
                 self.scalar_encapsed(expr['expr'], expr['var']['name'], '{scope}:VARIABLE'.format(scope=parent_node), 'ASSIGNS', scope)
+    elif expr['nodeType'] == 'Expr_StaticCall':
+        if expr['class']['nodeType'] == 'Name':
+            if "".join(expr['class']['nodeType']) == 'self':
+                self.expr_static_call(expr['name'], parent_node, parent_node_type, scope)
