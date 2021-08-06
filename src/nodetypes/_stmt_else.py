ad34e305eb89ef3439ae7254ffc4f34e609dd5ae
Nuwan Naththandige <nuwannc2@hotmail.com>
2021-08-07 04:14:04 +1200

    Change scope

--- src/nodetypes/_stmt_else.py
+++ src/nodetypes/_stmt_else.py
@@ -3,10 +3,10 @@ from utils.support import Print
 # Describes a Else block
 def stmt_else(self, slice, parent_node, parent_node_type, scope):
     if slice['attributes']['startLine']:
-        else_node_type = 'ELSE:{scope}'.format(parent_node=parent_node, scope=scope)
+        else_node_type = 'ELSE:{scope}'.format(scope=scope)
         else_node_name = self.generate_block_name_by_line(slice['attributes']['startLine'], 'ELSE')
 
-            # Create Else block node
+        # Create Else block node
         if not self.graph.find_node(else_node_name, else_node_type):
             self.graph.create_node(else_node_name, else_node_type)
 
@@ -19,6 +19,6 @@ def stmt_else(self, slice, parent_node, parent_node_type, scope):
             for stmt in slice['stmts']:
                 # If the statement is an echo
                 if stmt['nodeType'] == 'Stmt_Echo':
-                    self.stmt_echo(stmt['exprs'], else_node_name, else_node_type, '{this_node}:{parent_scope}'.format(this_node=else_node_name, parent_scope=scope))
+                    self.stmt_echo(stmt['exprs'], else_node_name, else_node_type, '{parent_scope}'.format(parent_scope=scope))
     else:
         Print.error_print('[404]', 'Issue in "Else[attributes]"')
