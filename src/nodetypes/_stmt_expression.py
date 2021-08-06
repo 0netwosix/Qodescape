08798608840dcf508ea0a8002873cd6e09d4a9d8
Nuwan Naththandige <nuwannc2@hotmail.com>
2021-08-07 04:15:06 +1200

    Add "expr_property_fetch" calls

--- src/nodetypes/_stmt_expression.py
+++ src/nodetypes/_stmt_expression.py
@@ -5,18 +5,40 @@ def stmt_expression(self, expr, parent_node, parent_node_type, scope):
         variable_decalred = False
         # Create the variable and its relationship with the parent node
         if expr['var']['nodeType'] == 'Expr_Variable':
-            self.expr_variable(expr, parent_node, parent_node_type, scope)
+            self.expr_variable(expr, parent_node, parent_node_type, scope, 'VARIABLE', 'IS_VARIABLE')
             variable_decalred = True
+
         # Describes "$this->variable = $variable;"
+        # and "$user->password = something();"
         elif expr['var']['nodeType'] == 'Expr_PropertyFetch':
+            # Describes "$this->variable = $variable;"
             if expr['var']['var']['nodeType'] == 'Expr_Variable' and expr['var']['var']['name'] == 'this':
                 tmp_expr = {
                     'var': {
                         'name': expr['var']['name']['name']
                     }
                 }
-                self.expr_variable(tmp_expr, parent_node, parent_node_type, scope)
+                self.expr_variable(tmp_expr, parent_node, parent_node_type, scope, 'VARIABLE', 'IS_VARIABLE')
                 variable_decalred = True
+
+            # Describes "$user->password = something();"
+            elif expr['var']['var']['nodeType'] == 'Expr_Variable' and expr['var']['var']['name'] != 'this':
+                '''
+                    Describes the following scenario
+                    e.g. $user->password = something()
+
+                    Parent node     = $user ('VARIABLE') ('GLOBAL_VARIABLE') ('PARAM')
+                    Child node      = password ('PROPERTY')
+                    Relationship    = $user -['IS_PROPERTY']-> password
+                '''
+                # Create the node and relationship
+                self.expr_property_fetch_lhs(expr['var'], scope)
+
+                # Handle the assigned value for this nodeType seperately in here
+                # Therefore "variable_declared" should be False
+                if expr['expr']['nodeType'] == 'Expr_FuncCall':
+                    self.expr_func_call(expr['expr'], expr['var']['name']['name'], '{scope}:PROPERTY'.format(scope=scope), scope, 'ASSIGNS')
+                
 
         ''' Following describes the value of above variable
         '''
@@ -35,6 +57,9 @@ def stmt_expression(self, expr, parent_node, parent_node_type, scope):
             # If the value of the above variable is similar to "$GET['id']"
             elif expr['expr']['nodeType'] == 'Expr_ArrayDimFetch':
                 self.expr_array_dim_fetch(expr['expr'], expr['var']['name'], '{scope}:VARIABLE'.format(scope=parent_node), 'ASSIGNS', scope, expr['expr']['var']['name'])
+            # If the value of the above variable is similar to "$user->password"
+            elif expr['expr']['nodeType'] == 'Expr_PropertyFetch':
+                self.expr_property_fetch_rhs(expr['expr'], expr['var']['name'], '{scope}:VARIABLE'.format(scope=parent_node), scope, 'ASSIGNS')
             # If the value of the above variable is similar to "select * from `products` where productCode='$prodCode'"
             elif expr['expr']['nodeType'] == 'Scalar_Encapsed':
                 self.scalar_encapsed(expr['expr'], expr['var']['name'], '{scope}:VARIABLE'.format(scope=parent_node), 'ASSIGNS', scope)
@@ -46,7 +71,7 @@ def stmt_expression(self, expr, parent_node, parent_node_type, scope):
     elif expr['nodeType'] == 'Expr_AssignOp_Concat':
         if expr['var']['nodeType'] == 'Expr_Variable':
             # Create the variable and its relationship with the parent node
-            self.expr_variable(expr, parent_node, parent_node_type, scope)
+            self.expr_variable(expr, parent_node, parent_node_type, scope, 'VARIABLE', 'IS_VARIABLE')
 
             ''' Following describes the value of above variable
             '''
