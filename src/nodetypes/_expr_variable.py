b8a729e0845d6a683a9bc26ca95792a29cee41dc
Nuwan Naththandige <nuwannc2@hotmail.com>
2021-08-07 04:13:10 +1200

    Add new parameters

--- src/nodetypes/_expr_variable.py
+++ src/nodetypes/_expr_variable.py
@@ -1,9 +1,9 @@
 # Create a variable and its relationship
-def expr_variable(self, expr, parent_node, parent_node_type, scope):
+def expr_variable(self, expr, parent_node, parent_node_type, scope, variable_type, relationship_type):
     # Create the variable implementing a relationship with the parent
-    if not self.graph.find_node(expr['var']['name'], '{scope}:VARIABLE'.format(scope=scope)):
-        self.graph.create_node(expr['var']['name'], '{scope}:VARIABLE'.format(scope=scope))
+    if not self.graph.find_node(expr['var']['name'], '{scope}:{variable_type}'.format(scope=scope, variable_type=variable_type)):
+        self.graph.create_node(expr['var']['name'], '{scope}:{variable_type}'.format(scope=scope, variable_type=variable_type))
 
     # Create the relationship with the parent
-    if not self.graph.find_relationship(parent_node, parent_node_type, expr['var']['name'], '{scope}:VARIABLE'.format(scope=scope), 'IS_VARIABLE'):
-        self.graph.create_relationship(parent_node, parent_node_type, expr['var']['name'], '{scope}:VARIABLE'.format(scope=scope), 'IS_VARIABLE')
+    if not self.graph.find_relationship(parent_node, parent_node_type, expr['var']['name'], '{scope}:{variable_type}'.format(scope=scope, variable_type=variable_type), relationship_type):
+        self.graph.create_relationship(parent_node, parent_node_type, expr['var']['name'], '{scope}:{variable_type}'.format(scope=scope, variable_type=variable_type), relationship_type)
