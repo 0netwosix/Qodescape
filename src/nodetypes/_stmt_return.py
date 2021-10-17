097e53c5e4c49eb9df71c9dc1e45614cee68d599
Nuwan Naththandige <nuwannc2@hotmail.com>
2021-10-18 11:18:42 +1300

    Add description and change to IS_FUNCTION_CALL

--- src/nodetypes/_stmt_return.py
+++ src/nodetypes/_stmt_return.py
@@ -4,13 +4,16 @@
     e.g. return testFunc($user, $password);
     e.g. return false;
 
+    TODO:
+        1. Better to create a seperate relationship "return" to denote this.
+
     HOW IT WORKS!
     1.) Based on the nodeType it calls the relavant nodeType method to establish the node. 
 '''
 def stmt_return(self, expr, parent_node, parent_node_type, scope):
     # If the retrun statement is a Function call
     if expr['nodeType'] == 'Expr_FuncCall':
-        self.expr_func_call(expr, parent_node, parent_node_type, scope, 'FUNCTION_CALL')
+        self.expr_func_call(expr, parent_node, parent_node_type, scope, 'IS_FUNCTION_CALL')
     # If the retrun statement is a Method call
     elif expr['nodeType'] == 'Expr_MethodCall':
         self.expr_method_call(expr, parent_node, parent_node_type, scope)
